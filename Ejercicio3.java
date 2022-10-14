1529e9af280c70c747c49d0126d5a8908664eb97
Adrian <chopon007@gmail.com>
2022-10-14 11:29:36 +0200

    Ejercicio3 ready

---
+++ Ejercicio3.java
@@ -0,0 +1,21 @@
+/*
+(づ ◕‿◕ )づ
+    ************************************************************************************
+    *                                                                                  *
+    *         4.   Sentencia Condicional                                               *
+    *                                                                                  *
+    *         3.   Escribe un programa en que dado un número del 1 a 7 escriba el      *
+    *              correspondiente nombre del día de la semana.                        *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        
+    }
+}
