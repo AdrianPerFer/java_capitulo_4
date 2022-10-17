f48cb9552bd51b96a52060ac7d818a0e41a54626
Adrian <chopon007@gmail.com>
2022-10-17 08:26:07 +0200

    Ejercicio2 added

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -17,8 +17,24 @@
     *                                                             ******               *
     ************************************************************************************
 */
+import java.util.Scanner;
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca una hora del día (0-23): ");
+        int hora = sc.nextInt();
+        if ((hora >= 6) && (hora <= 12)) {
+            System.out.println("Buenos días");
+        }
+        if ((hora >= 13) && (hora <= 20)) {
+            System.out.println("Buenas tardes");
+        }
+        if (((hora >= 21) && (hora < 24)) || ((hora <= 5) && (hora >= 0))) {
+            System.out.println("Buenas noches");
+        }
+        if ((hora >= 24) || (hora < 0)) {
+            System.out.println("La hora introducida es erronea.");
+        }
+        sc.close();   
     }
 }
