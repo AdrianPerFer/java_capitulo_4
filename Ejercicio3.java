2607c11b090aaaed69c2fedd6d1ce5849b89cafb
Adrian <chopon007@gmail.com>
2022-10-17 08:31:02 +0200

    Ejercicio3 added

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -14,8 +14,38 @@
     *                                                             ******               *
     ************************************************************************************
 */
+import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String dia;
+        System.out.println("Introduzca un número del 1 al 7: ");
+        int numero = sc.nextInt();
+        switch(numero) {
+            case 1:
+                dia = "Lunes";
+                break;
+            case 2:
+                dia = "Martes";
+                break;
+            case 3:
+                dia = "Miércoles";
+                break;
+            case 4:
+                dia = "Jueves";
+                break;
+            case 5:
+                dia = "Viernes";
+                break;
+            case 6:
+                dia = "Sábado";
+                break;
+            case 7:
+                dia = "Domingo";
+                break;
+            default:
+                dia = "Debe introducir un número del 1 al 7";
+        }
+        System.out.println(dia);
     }
 }
