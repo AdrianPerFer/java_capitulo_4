a9e254a47d9606acc46a100681ca055b6bfdb4f1
Adrian <chopon007@gmail.com>
2022-10-17 09:49:51 +0200

    Ejercicio17 added

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -1,3 +1,26 @@
+/*
+(づ ◕‿◕ )づ
+    ************************************************************************************
+    *                                                                                  *
+    *         4.   Sentencia Condicional                                               *
+    *                                                                                  *
+    *         17.  Escribe un programa que diga cuál es la última cifra de un número   *
+    *              entero introducido por teclado.                                     *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
+import java.util.Scanner;
 public class Ejercicio17 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un número entero: ");
+        int numero = sc.nextInt();
+        System.out.println("La última cifra es " + (numero % 10));
+        sc.close();
+   }
 }
