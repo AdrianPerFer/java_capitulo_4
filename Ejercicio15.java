91c0a55fef1980f0f2294bd0c387516a96abd544
Adrian <chopon007@gmail.com>
2022-10-17 09:28:37 +0200

    Ejercicio15 added

--- Ejercicio15.java
+++ Ejercicio15.java
@@ -4,7 +4,7 @@
     *                                                                                  *
     *         4.   Sentencia Condicional                                               *
     *                                                                                  *
-    *         13.  Escribe un programa que pinte una pirámide rellena con un carácter  *
+    *         15.  Escribe un programa que pinte una pirámide rellena con un carácter  *
     *              introducido por teclado que podrá ser una letra, un número o un     *
     *              símbolo como *, +, -, $, &, etc. El programa debe permitir al       *
     *              usuario mediante un menú elegir si el vértice de la pirámide está   *
